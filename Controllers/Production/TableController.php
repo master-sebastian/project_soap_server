6b4da687e3a24b3947cc74d8fc754d93c75c8008
Sebastian G <orjuela.desarrollador@gmail.com>
2020-04-18 20:46:40 -0500

    Login para mesas

--- Controllers/Production/TableController.php
+++ Controllers/Production/TableController.php
@@ -22,11 +22,51 @@
 				}
 				return ['not fount',$method_name, $arguments];
 			}else{
+				
+				if($method_name  == "loginTable"){
+					return $this->loginTable($arguments);
+				}
 				return [
 					'status' => 'error-autentication',
 					'message' => 'No tiene la autenticacion requerida para acceder a este modulo'
 				];
 			}
+		}
+
+		private function loginTable($arguments){
+			require_once '../SegurityApp.php';
+			require_once '../Models/User.php';
+
+			$nombre = $arguments->nombre;
+			$clave = $arguments->clave;
+			$existeMesa = new Table();
+
+			$resultadoDeLaConsulta = $existeMesa->select(["*"], "nombre = '".$nombre."'");
+			if(count($resultadoDeLaConsulta) > 0){
+				$resultadoDeLaConsulta = $existeMesa->select(["*"], "nombre = '".$nombre."' AND token ='".$clave."'");
+				if(count($resultadoDeLaConsulta) > 0){
+					return [
+						'status'=> 'success',
+						'token' => SegurityApp::encriptar(json_encode(
+							array(
+								"idMesa" => $resultadoDeLaConsulta,
+								"token"=> sha1($clave)
+							)
+						))
+					];;
+				}else{
+					return [
+						'status' => 'error',
+						'message' => 'La clave es incorrecta',
+					];
+				}
+			}else{
+				return [
+					'status' => 'error',
+					'message' => 'La mesa no esta registrado en el sistema',
+				];;
+			}
+			
 		}
 
 		private function editTable($arguments){
